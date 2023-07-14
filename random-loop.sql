a4e5cb743367c66449110486d30d7a11068bf7cb
Ilnar Shamionov <ilnar_08@mail.ru>
2023-07-14 14:57:31 +0400

    Исправил кодировку

--- random-loop.sql
+++ random-loop.sql
@@ -1,22 +1,22 @@
----Вставка в цикле
+---INSERT РІ С†РёРєР»Рµ
 DECLARE @i INT = 1
 
 WHILE @i <= 1000000
 BEGIN
   INSERT INTO transactions(Record)
-  VALUES ('Текстовая запись ' + CAST(@i AS NVARCHAR(20)))
+  VALUES ('РўРµРєСЃС‚РѕРІР°СЏ Р·Р°РїРёСЃСЊ ' + CAST(@i AS NVARCHAR(20)))
 
   SET @i = @i + 1
 END
 
---- Обновление в цикле
+---UPDATE РІ С†РёРєР»Рµ
 DECLARE @i INT = 1
 
 WHILE @i <= 10000 
 BEGIN
 	UPDATE transactions
-	SET Record = REPLACE(Record, 'Текстовая запись ' 	+ CAST(@i AS NVARCHAR(20)), 
-	                             'Замена текстовой записи ' + CAST(@i AS NVARCHAR(20)))
-	WHERE Record = 'Текстовая запись ' 	+ CAST(@i AS NVARCHAR(20)) 
+	SET Record = REPLACE(Record, 'РўРµРєСЃС‚РѕРІР°СЏ Р·Р°РїРёСЃСЊ ' 	+ CAST(@i AS NVARCHAR(20)), 
+	                             'РР·РјРµРЅРµРЅРёРµ С‚РµРєСЃС‚РѕРІРѕР№ Р·Р°РїРёСЃРё ' + CAST(@i AS NVARCHAR(20)))
+	WHERE Record = 'РўРµРєСЃС‚РѕРІР°СЏ Р·Р°РїРёСЃСЊ ' 	+ CAST(@i AS NVARCHAR(20)) 
 	SET @i = @i + 1
 END
